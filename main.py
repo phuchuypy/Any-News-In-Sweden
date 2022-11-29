e7b90267dbf16004970e0af0f4829b28ec4f1843
phuchuypy <phuchuypy12@icloud.com>
2022-11-29 20:39:13 +0100

    Change the seperator of titles from , to \n, since , might be in the title

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ news_source = "https://www.thelocal.se/category/sweden-news/"
 file_name = "news_titles.txt"
 
 def main():
-    news_titles = get_news_titles(news_source)
+    today_titles, urls = get_news_titles(news_source)
     if os.path.isfile("./" + file_name):
-        new_titles, urls = compare_news_titles(news_titles,file_name)
+        new_titles = compare_news_titles(today_titles,file_name)
         for title in new_titles:
             print(title)
         for url in urls:
             webbrowser.open(url)
-    #save_news_titles_to_file(news_titles)
+    save_news_titles_to_file(today_titles)
 
 def get_news_titles(url):
     headers = {"User-Agent": "Mozilla/5.0"}
     req = Request(url, headers=headers)
     webpage = urlopen(req).read()
     page_soup = BeautifulSoup(webpage, "html.parser")
-    # TODO: change the below attribute(s) depending on different news sources
+    # TODO: change the below attribute(s) depending on news website
     # The purpose here is to get all of the titles of the articles
     news_links = page_soup.findAll("a",attrs={"class":"article-link-event"})
 
@@ -36,22 +36,22 @@ def get_news_titles(url):
             urls.append(url)
     return titles, urls
 
+def compare_news_titles(today_titles,file):
+    new_titles = []
+    yesterday_titles = open(file, "r").read()
+    yesterday_titles = yesterday_titles.split("\n")
+    for index in range(len(today_titles)):
+        if today_titles[index] not in yesterday_titles:
+            new_titles.append(today_titles[index])
+    return new_titles
+    
 def save_news_titles_to_file(titles):
     file = open(file_name,"w")
     for index in range(len(titles)):
         if index == len(titles) - 1:
             file.write(f"{titles[index]}")
         else:
-            file.write(f"{titles[index]},")
+            file.write(f"{titles[index]}\n")
     file.close()
 
-def compare_news_titles(today_titles,file):
-    new_titles = []
-    yesterday_titles = open(file, "r").read()
-    yesterday_titles = yesterday_titles.split(",")
-    for index in range(len(today_titles)):
-        if today_titles[index] not in yesterday_titles:
-            new_titles.append(today_titles[index])
-    return new_titles
-    
 main()
